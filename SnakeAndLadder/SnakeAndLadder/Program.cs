627a7e1b08803cd2993a0bb120cd8a97364a883d
shital bramhe <shitalbramhe@gmail.com>
2021-11-09 15:04:07 +0530

    [shital] Add : initial

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder Program");
+            SinglePlayer Player = new SinglePlayer();
+            Player.StartPlaying();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/SinglePlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SinglePlayer
+    {
+        public int StartPoint = 0;
+        public void StartPlaying()
+        {
+            Console.WriteLine("Game Started! Good Luck "); 
+
+            Console.WriteLine("Starting point is " + StartPoint); //Main Start Point
+        }
+    }
+}
